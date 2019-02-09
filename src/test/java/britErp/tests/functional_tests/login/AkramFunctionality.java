7e23e1b2263009ce20b47b11897e8d7a71747765
Akramjava <akramttaa@gmail.com>
2019-02-08 21:58:49 -0800

    Akram's commit

---
+++ src/test/java/britErp/tests/functional_tests/login/AkramFunctionality.java
@@ -0,0 +1,10 @@
+package britErp.tests.functional_tests.login;
+
+import britErp.utilities.TestBase;
+import org.testng.annotations.Test;
+
+public class AkramFunctionality  extends TestBase {
+
+
+    }
+
